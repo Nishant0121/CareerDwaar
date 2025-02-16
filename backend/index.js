30112a625ee574288f00dfdf235b793336ae4e8a
Nishant0121 <142099904+Nishant0121@users.noreply.github.com>
2025-02-16 12:48:48 +0530

    back-front

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:3000",
-      "https://careerdwaar.onrender.com",
+      "https://career-dwaar.vercel.app",
     ],
     credentials: true,
   })
